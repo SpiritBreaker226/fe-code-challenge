c15b8c5779e3500b2af0c7684d22e6a9beabbb34
Jason Stathopulos <jstathopulos@gmail.com>
2020-11-04 21:36:29 -0500

    Add Fourm to Checkout Component
    Add a forum to Checkout so that the user can purchase a spot and go to
    the confirmation screen.

--- src/js/checkout/Checkout.jsx
+++ src/js/checkout/Checkout.jsx
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 
 import {connect} from 'react-redux';
 
+import {useFormik} from 'formik';
+
 import {push} from 'connected-react-router';
 
 import Button from '../common/Button';
@@ -10,15 +12,53 @@ import TextButton from '../common/TextButton';
 
 import SpotItem from '../spot/SpotItem';
 
-const Checkout = ({selectedSpot, pushTo}) => {
-    const onPurchaseClick = () => pushTo('/confirmation');
+import {purchase} from './checkout-actions';
+
+const validate = values => {
+    const errors = {};
+
+    if (!values.phone) {
+        errors.phone = 'Phone is Required';
+    } else if (!/^[0-9]{10}$/i.test(values.phone)) {
+        errors.phone = 'Please enter a valid phone number.';
+    }
+
+    if (!values.email) {
+        errors.email = 'Email is Required';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = 'Please enter a valid email.';
+    }
+
+    return errors;
+};
+
+const Checkout = ({selectedSpot, pushTo, onCheckout}) => {
+    const formik = useFormik({
+        initialValues: {
+            firstName: '',
+            lastName: '',
+            email: '',
+            phone: '',
+        },
+        validate,
+        onSubmit: values => {
+            onCheckout(values);
+            pushTo('/confirmation');
+        },
+    });
+    const onFieldAction = type => (
+        (type === 'submit') ? formik.handleSubmit : formik.handleChange
+    );
     const isSpotSelected = (selectedSpot && Object.entries(selectedSpot).length > 0);
+    const onBackToSearchClick = () => {
+        pushTo('/');
+    };
 
     return (
         <section className="checkout">
             <header>
                 <nav className="checkout-nav">
-                    <TextButton>Back to Search</TextButton>
+                    <TextButton onClick={onBackToSearchClick}>Back to Search</TextButton>
                 </nav>
 
                 {
@@ -33,13 +73,53 @@ const Checkout = ({selectedSpot, pushTo}) => {
 
             {
                 isSpotSelected ?
-                    <Button
-                        data-testid="purchase-spot"
-                        color="secondary"
-                        onClick={onPurchaseClick}
-                    >
-                        Purchase for $14.00
-                    </Button>
+                    <form onSubmit={onFieldAction('submit')}>
+                        <label htmlFor="firstName">First Name</label>
+                        <input
+                            id="firstName"
+                            name="firstName"
+                            type="text"
+                            onChange={onFieldAction('field')}
+                            value={formik.values.firstName}
+                        />
+                        {formik.errors.firstName ? <div>{formik.errors.firstName}</div> : null}
+                        <label htmlFor="lastName">Last Name</label>
+                        <input
+                            id="lastName"
+                            name="lastName"
+                            type="text"
+                            onChange={onFieldAction('field')}
+                            value={formik.values.lastName}
+                        />
+                        {formik.errors.lastName ? <div>{formik.errors.lastName}</div> : null}
+                        <label htmlFor="email">Email</label>
+                        <input
+                            id="email"
+                            name="email"
+                            type="text"
+                            data-testid="purchase-spot-email"
+                            onChange={onFieldAction('field')}
+                            value={formik.values.email}
+                        />
+                        {formik.errors.email ? <div data-testid="purchase-spot-email-error">{formik.errors.email}</div> : null}
+                        <label htmlFor="phone">Phone</label>
+                        <input
+                            id="phone"
+                            name="phone"
+                            type="text"
+                            data-testid="purchase-spot-phone"
+                            onChange={onFieldAction('field')}
+                            value={formik.values.phone}
+                        />
+                        {formik.errors.phone ? <div data-testid="purchase-spot-phone-error">{formik.errors.phone}</div> : null}
+                        <Button
+                            data-testid="purchase-spot-submit"
+                            color="secondary"
+                            type="submit"
+                        >
+                            Purchase for ${(selectedSpot.price / 100).toFixed(2)}
+                        </Button>
+                    </form>
                     :
                     <span>Select a Spot by going back to Search</span>
             }
@@ -61,9 +141,11 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = {
     pushTo: push,
+    onCheckout: purchase,
 };
 
 Checkout.propTypes = {
+    onCheckout: PropTypes.func.isRequired,
     pushTo: PropTypes.func.isRequired,
     selectedSpot: PropTypes.object,
 };
--- src/js/checkout/Checkout.test.jsx
+++ src/js/checkout/Checkout.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {render} from '@testing-library/react';
+import {fireEvent, render, screen, waitFor} from '@testing-library/react';
 
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
@@ -12,6 +12,10 @@ import spotReducer, {
 } from '../spot/spot-reducer';
 
 describe('Checkout', () => {
+    const elementToRender = testId => {
+        return () => screen.getByTestId(testId);
+    };
+
     it('should render a fourm with the spot details', () => {
         const spotId = faker.random.number(9);
         const mockStore = createStore(
@@ -42,6 +46,69 @@ describe('Checkout', () => {
         ).not.toBeInTheDocument();
 
         expect(queryByTestId(`spot-item-info-${spotId}`)).toBeInTheDocument();
+        expect(queryByTestId('purchase-spot-submit')).toBeInTheDocument();
+    });
+
+    describe('using forum', () => {
+        let mockStore = null;
+
+        beforeEach(() => {
+            mockStore = createStore(
+                spotReducer,
+                {
+                    ...initialState,
+                    spot: {
+                        selected: {
+                            id: faker.random.number(9),
+                            title: faker.lorem.lines(1),
+                            price: faker.random.number(9999),
+                            description: faker.lorem.paragraph(1),
+                            distance: `${faker.random.number(999)} ft`,
+                            image: faker.image.city()
+                        },
+                    },
+                },
+            );
+
+            render(
+                <Provider store={mockStore}>
+                    <Checkout />
+                </Provider>
+            );
+        });
+
+        it('should display error messages for phone & email', async () => {
+            fireEvent.click(screen.getByTestId('purchase-spot-submit'));
+
+            await waitFor(elementToRender('purchase-spot-submit'));
+
+            expect(screen.queryByTestId('purchase-spot-email-error')).toHaveTextContent(
+                /Required/i
+            );
+            expect(screen.queryByTestId('purchase-spot-phone-error')).toHaveTextContent(
+                /Required/i
+            );
+
+            fireEvent.change(screen.getByTestId('purchase-spot-email'), {
+                target: {value: faker.name.firstName()},
+            });
+
+            await waitFor(elementToRender('purchase-spot-email'));
+
+            expect(screen.queryByTestId('purchase-spot-email-error')).toHaveTextContent(
+                /valid email/i
+            );
+
+            fireEvent.change(screen.getByTestId('purchase-spot-phone'), {
+                target: {value: faker.name.jobTitle()},
+            });
+
+            await waitFor(elementToRender('purchase-spot-phone'));
+
+            expect(screen.queryByTestId('purchase-spot-phone-error')).toHaveTextContent(
+                /valid phone number/i
+            );
+        });
     });
 
     describe('when user goes directly to the checkout from URL', () => {
@@ -50,7 +117,7 @@ describe('Checkout', () => {
                 spotReducer,
                 {
                     ...initialState,
-                    spot: {selected: {}},
+                    spot: {selected: null},
                 },
             );
 
@@ -64,7 +131,7 @@ describe('Checkout', () => {
                 queryByText('Select a Spot', {exact: false})
             ).toBeInTheDocument();
 
-            expect(queryByTestId('purchase-spot')).not.toBeInTheDocument();
+            expect(queryByTestId('purchase-spot-submit')).not.toBeInTheDocument();
         });
     });
 });
